00fe4e33eab443d3a9e3d12cee34ffe453f725ea
ThisNarciss <tyomaxxx000@gmail.com>
2023-08-01 11:19:18 +0300

    add header search logic and dropdown

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './ts/api/fetchingComics';
 import './ts/change-header-bg';
+import './ts/search-comics';
 import './ts/slider-character';
 import './ts/slider-last-comics';
 import './ts/modal';
---
+++ src/ts/search-comics.ts
@@ -0,0 +1,28 @@
+import { searchComics } from './api/fetchingComics';
+import { dropdownMarkUp } from './dropdown-mark-up';
+
+const searchInput = document.querySelector('.search_input') as HTMLInputElement;
+const searchForm = document.querySelector('.search_form') as HTMLFormElement;
+
+searchInput.addEventListener('input', handleSearch);
+searchForm.addEventListener('submit', handleSubmit);
+
+function handleSearch(e: Event) {
+  const { value } = e.currentTarget as HTMLInputElement;
+  searchComics(value.trim()).then(data => {
+    dropdownMarkUp(data);
+  });
+}
+
+function handleSubmit(e: Event) {
+  e.preventDefault();
+  const form = e.currentTarget as HTMLFormElement;
+  const inputElement = form.elements.namedItem('fav') as HTMLInputElement;
+
+  const value: string = inputElement.value.trim();
+  localStorage.setItem('searchComic', value);
+
+  if (location.pathname === '/index.html' && value) {
+    location.assign('./comics.html');
+  }
+}
