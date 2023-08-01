662861015d70d9c76ece8add4f3bff9b7304747b
ThisNarciss <tyomaxxx000@gmail.com>
2023-08-01 12:50:53 +0300

    add comics hero mark up and style

--- src/ts/comics-last-week-mark-up.ts
+++ src/ts/comics-last-week-mark-up.ts
@@ -7,11 +7,17 @@ const lastComicsSlideTwoRef = document.querySelector(
   '.slide-two'
 ) as HTMLDivElement;
 
+type Creator = {
+  name: string;
+  resourceURI: string;
+  role: string;
+};
+
 type Item = {
   id: number;
   thumbnail: { path: string; extension: string };
   title: string;
-  creators: { items: any[] };
+  creators: { items: Creator[] };
 };
 
 interface IData {
@@ -21,12 +27,24 @@ interface IData {
 export const createLastComicsMurkUp = (data: IData) => {
   const { results } = data;
 
+  const writer = (arr: any[]) => {
+    const name = arr.find((item: Creator) => item.role === 'writer');
+
+    return name;
+  };
+
   const murkUpArr = results.map(item => {
     return `
         <li class="last-comics_list_item">
-        <article><img id=${item.id}  class="last-comics_list_img" src="${item.thumbnail.path}/portrait_uncanny.${item.thumbnail.extension}" alt="comics article" loading="lazy" width="448" height="519">
+        <article><img id=${item.id}  class="last-comics_list_img" src="${
+      item.thumbnail.path
+    }/portrait_uncanny.${
+      item.thumbnail.extension
+    }" alt="comics article" loading="lazy" width="448" height="519">
         <h3 class="last-comics_list_title">${item.title}</h3>
-        <p class="last-comics_list_text">${item.creators.items[0]?.name}</p></article>
+        <p class="last-comics_list_text">${
+          writer(item.creators.items)?.name ?? 'No info'
+        }</p></article>
         
       </li>
     `;
