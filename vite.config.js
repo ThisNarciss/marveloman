b7bb41eac9a1fb6bb162f05b50ded557c43f2a36
ThisNarciss <tyomaxxx000@gmail.com>
2023-08-01 13:44:34 +0300

    fix location

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,17 @@ import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
 
 export default {
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        nested: 'comics.html',
+      },
+    },
+  },
   plugins: [
     handlebars({
-      partialDirectory: resolve(
-        `${__dirname}/src`,
-        'partials'
-      ),
+      partialDirectory: resolve(`${__dirname}/src`, 'partials'),
     }),
   ],
 };
